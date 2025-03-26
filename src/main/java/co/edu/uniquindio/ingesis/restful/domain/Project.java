523133fe59b7fc552aaa5af5865da2ea4a822d58
Jose A. Zapata <josezapata75235@gmail.com>
2025-03-25 20:02:34 -0500

    Add consul

--- src/main/java/co/edu/uniquindio/ingesis/restful/domain/Project.java
+++ src/main/java/co/edu/uniquindio/ingesis/restful/domain/Project.java
@@ -1,9 +1,7 @@
 package co.edu.uniquindio.ingesis.restful.domain;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -16,17 +14,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class Project extends PanacheEntity {
-
     @Size(min = 3, max = 50, message = "The title must have between 3 and 50 characters.")
     @NotBlank(message = "The title is required.")
     private String title;
-
     @Size(max = 255, message = "The description cannot exceed 255 characters.")
     private String description;
-
     @NotBlank(message = "The source code is required.")
     private String code;
-
     private LocalDateTime createdAt;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
 
 }
